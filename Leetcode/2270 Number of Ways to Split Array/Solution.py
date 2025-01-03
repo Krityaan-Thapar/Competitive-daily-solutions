c08e861999cf9a59eae88d9c88fae453f0f1bec7
Krityaan Thapar <kritthapar@gmail.com>
2025-01-03 09:42:56 +0530

    Extra solve

--- Leetcode/2270 Number of Ways to Split Array/Solution.py
+++ Leetcode/2270 Number of Ways to Split Array/Solution.py
@@ -11,3 +11,14 @@ class Solution:
             if prefix[i] >= suffix[i]:
                 ans += 1
         return ans
+
+class Solution2:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        l, r = 0, sum(nums)
+        ans = 0
+        for i in range(len(nums) - 1):
+            l += nums[i]
+            r -= nums[i]
+            if l >= r:
+                ans += 1
+        return ans
