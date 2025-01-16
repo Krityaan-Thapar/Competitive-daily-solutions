1d1a02759a830f9ec71b0a8e1c67d57e2ef508a2
Krityaan Thapar <thaparkt@amazon.com>
2025-01-16 17:14:31 +0530

    I DID IT

--- Leetcode/3418 Maximum Amount of Money Robot Can Earn/Solution.py
+++ Leetcode/3418 Maximum Amount of Money Robot Can Earn/Solution.py
@@ -52,3 +52,35 @@ class Solution2:
                         down = max(cache[i + 1][j][k - 1], down) if i + 1 < r else -int(1e15)
                     cache[i][j][k] = max(right, down)
         return cache[0][0][2]
+
+class Solution3:
+    def maximumAmount(self, coins: List[List[int]]) -> int:
+        r, c = len(coins), len(coins[0])
+        curr = [[-int(1e15) for _ in range(3)] for _ in range(c)]
+
+        for k in range(3):
+            curr[0][k] = coins[0][0]
+        if coins[0][0] < 0:
+            curr[0][0] = 0
+            curr[0][1] = 0
+        
+        for j in range(1, c):
+            for k in range(3):
+                curr[j][k] = max(curr[j][k], curr[j - 1][k] + coins[0][j])
+                if coins[0][j] < 0 and k > 0:
+                    curr[j][k - 1] = max(curr[j][k - 1], curr[j - 1][k])
+        
+        for i in range(1, r):
+            prev = [row[:] for row in curr]
+            curr = [[-int(1e15) for _ in range(3)] for _ in range(c)]
+            for k in range(3):
+                curr[0][k] = max(curr[0][k], prev[0][k] + coins[i][0])
+                if coins[i][0] < 0 and k > 0:
+                    curr[0][k - 1] = max(curr[0][k - 1], prev[0][k])
+            
+            for j in range(1, c):
+                for k in range(3):
+                    curr[j][k] = max(curr[j][k], curr[j - 1][k] + coins[i][j], prev[j][k] + coins[i][j])
+                    if coins[i][j] < 0 and k > 0:
+                        curr[j][k - 1] = max(curr[j][k - 1], curr[j - 1][k], prev[j][k])
+        return max(curr[-1])
