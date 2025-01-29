00b6ecd415825d899cf086ec1ebbc49a022a8c43
Krityaan Thapar <kritthapar@gmail.com>
2025-01-29 12:18:32 +0530

    Forgot to commit code

--- Leetcode/684 Redundant Connection/Solution.py
+++ Leetcode/684 Redundant Connection/Solution.py
@@ -1,22 +1,38 @@
 from typing import List
 
-class Solution:
-    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
-        stk = []
-        ans = [0 for _ in range(n)]
-        prev = 0
+class UnionFind:
+    def __init__(self):
+        self.id = {}
+        self.rank = {}
+    
+    def find(self, x):
+        y = self.id.get(x, x)
+        if y != x:
+            self.id[x] = y = self.find(y)
+        return y
+    
+    def union(self, x, y):
+        if self.find(x) not in self.rank:
+            self.rank[self.find(x)] = 0
+        
+        if self.find(y) not in self.rank:
+            self.rank[self.find(y)] = 0
+        
+        if self.rank[self.find(x)] < self.rank[self.find(y)]:
+            self.id[self.find(x)] = self.find(y)
+        else:
+            self.id[self.find(y)] = self.find(x)
+            if self.rank[self.find(x)] == self.rank[self.find(y)]:
+                self.rank[self.find(x)] = self.rank[self.find(x)] + 1
 
-        for i in logs:
-            pid, op, time = i.split(":")
-            pid = int(pid)
-            time = int(time)
-            
-            if op == "end":
-                ans[stk.pop()] += time - prev + 1
-                prev = time + 1
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        dsu = UnionFind()
+        ans = None
+
+        for s, e in edges:
+            if dsu.find(e) == dsu.find(s):
+                ans = [s, e]
             else:
-                if stk:
-                    ans[stk[-1]] += time - prev
-                stk.append(pid)
-                prev = time
+                dsu.union(s, e)
         return ans
