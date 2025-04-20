a480df49a2457a08181c6e96b1352128bb70afd3
Krityaan Thapar <thaparkt@amazon.com>
2025-04-20 20:02:21 +0530

    GFG 19 Apr

---
+++ GFG/Maximum XOR of two numbers in an array/Solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxXor(self, arr):
+        res = 0
+        mask = 0
+        s = set()
+        
+        for i in range(30, -1, -1):
+            mask |= (1 << i)
+            for num in arr:
+                s.add(num & mask)
+                
+            cur = res | (1 << i)
+            for prefix in s:
+                if cur ^ prefix in s:
+                    res = cur
+                    break
+            s.clear()
+        return res
+
